8a40271fe401557032532d53ad2a32b503d27930
yaojijiayou <yaoji_nju@163.com>
2016-08-17 17:17:35 +0800

    Update Normaliser.js

--- Normaliser.js
+++ Normaliser.js
@@ -2,19 +2,18 @@ function Normaliser(){
     var NORMALOBJ = {};
 
     this.normalize = function(str,callback){
-        NORMALOBJ = {};
         if(!str) return;
+        NORMALOBJ = {};
         var lines = str.split('\n');
         for(var i = 0;i < lines.length;i++) normalizeOneLine(lines[i].trim());
         if(callback) callback(NORMALOBJ);
-        
     }
 
     function normalizeOneLine(lineStr){
         if(!lineStr) return;
         var strArr = lineStr.split("\t");
         if(strArr.length != 3) return;
-
+        
         var objName = strArr[0],fieldName = strArr[1],fieldType = strArr[2];
 
         if(!NORMALOBJ[objName]) NORMALOBJ[objName] = objName+"#";
